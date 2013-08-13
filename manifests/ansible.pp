73b544632240d95d881ca9d0134faf97a5682265
Jason Brown <jbrown@edac.unm.edu>
2013-08-13 16:01:35 -0600

    Pushing to all nodes

--- manifests/ansible.pp
+++ manifests/ansible.pp
@@ -25,27 +25,56 @@ from your host machine. It is running ${operatingsystem} ${operatingsystemreleas
 Puppet ${puppetversion}.  ",
 }
 
-package{'ansible':
-    ensure => present
+package{'python-dev':
+    # Needed for pip to build ansible
+    ensure => present,
 }
 
-package{'ansible-doc':
-    ensure => present
+package{'python-pip':
+    ensure => present,
+    # not a python server, but we need pip to get ansible instead of system packages
 }
 
-package{'ansible-fireball':
-    ensure => present
+exec{'get_pip_mongo':
+    # Note -- our default cwd for this happens to be in our manifsts folder,
+    #  making a build cache
+    command => '/usr/bin/sudo /usr/bin/pip install ansible',
+    require => [Package['python-pip'], Package['python-dev']],
 }
 
+# turn me back on
 # Server config hosts file
+file {'/etc/ansible':
+    ensure => directory,
+    owner => 'root',
+    force => true 
+}
+
 file {'/etc/ansible/hosts':
-    require => Package['ansible'],
+    #require => Package['ansible'],
+    require => Exec['get_pip_mongo'],
     ensure => link,
     target => '/vagrant/deployment/hosts',
     force => true,
+    owner => 'root',
 }
 
 # No clue how the hell ansible would work without ssh.
 #
 include ssh
 include packages
+
+#  Because these packages are ansible 1.1 in ubu 12.04 and aren't 1.2
+#  which is what we need to work with the examples.
+
+#package{'ansible':
+#    ensure => present
+#}
+#
+#package{'ansible-doc':
+#    ensure => present
+#}
+#
+#package{'ansible-fireball':
+#    ensure => present
+#}
