da8e90c4822934d8b3178a43508919c865d0635d
Jason Brown <jbrown@edac.unm.edu>
2013-08-22 17:02:39 -0600

    Ordering fixed with Class requirement

--- manifests/snode.pp
+++ manifests/snode.pp
@@ -1,5 +1,9 @@
 ## snode.pp ##
 
+include ssh
+include packages 
+include saltnode
+
 filebucket { 'main':
   path   => '/root/puppet_backup',
 }
@@ -10,13 +14,29 @@ File { backup => 'main' }
 file {'motd':
   ensure  => file,
   path    => '/etc/motd',
-  mode    => 0644,
-  content => "This is the salt server. IP address is ${ipaddress_eth1}. It thinks its
-hostname is ${fqdn}, but you might not be able to reach it there
-from your host machine. It is running ${operatingsystem} ${operatingsystemrelease} and
-Puppet ${puppetversion}.  ",
+  mode    => "0644",
+  content => "This is the salt server. IP address is ${ipaddress_eth1}. It
+  thinks its hostname is ${fqdn}, but you might not be able to reach it there
+  from your host machine. It is running ${operatingsystem}
+  ${operatingsystemrelease} and Puppet ${puppetversion}.  ",
 }
 
-include ssh
-include packages
-include saltnode
+# Punch out the defaults
+exec {'patch_salt_minion_conf':
+    command => "/usr/bin/sudo /bin/sed -i 's/^#master: salt$/master: salt.test.edac.unm.edu/' /etc/salt/minion",
+    require => [Class['saltnode']]
+} 
+
+file{ "/etc/salt/minion":
+    notify => Service['salt-minion'],
+    mode => "644",
+    owner => "root",
+    group => "root",
+    require => [Class['saltnode']]
+}
+
+service{'salt-minion':
+    ensure => "running",
+    require => [Class['saltnode']]
+}
+
